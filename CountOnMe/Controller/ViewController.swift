aaaa418eff54be08638d711340829935184b5f48
Nicolas HECKER <hecker.nicolas.pro@gmail.com>
2023-08-08 15:43:22 +0200

    Update: Added documentation

--- CountOnMe/Controller/ViewController.swift
+++ CountOnMe/Controller/ViewController.swift
@@ -9,38 +9,83 @@
 import UIKit
 
 class ViewController: UIViewController, CalculatorDelegate {
+    
+    // MARK: - viewDidLoad
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         calculator.delegate = self
     }
     
+    // MARK: - Delegate Pattern
+    
+    /**
+     This method is called to update the content of the text display in a UITextView.
+     
+     - Parameters:
+        - text: The text to be displayed in the UITextView.
+    */
     func updateDisplay(text: String) {
         textView.text = text
     }
     
+    /**
+     This method is used to display a modal alert with a given message.
+     
+     - Parameters:
+        - message: The message to be displayed in the alert.
+    */
     func displayAlert(message: String) {
         let alertVC = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
         alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
         return self.present(alertVC, animated: true, completion: nil)
     }
     
+    // MARK: - IBOutlet
+    
+    /**
+     This IBOutlet is used to establish a weak connection to a UITextView in the Interface Builder.
+     It allows the code to access and manipulate the properties and content of the connected UITextView.
+    */
     @IBOutlet weak var textView: UITextView!
     
+    /**
+     An instance of the Calculator class used for performing calculations and managing related functionality.
+    */
     private let calculator = Calculator()
     
-    // View actions
+    // MARK: - IBAction
+    
+    /**
+     IBAction triggered when a number button is tapped. It retrieves the title of the tapped button,
+     representing a number, and informs the calculator instance to process the tapped number.
+     
+     - Parameter sender: The UIButton that triggered the action.
+    */
     @IBAction func tappedNumberButton(_ sender: UIButton) {
         guard let numberText = sender.title(for: .normal) else { return }
         
         calculator.tappedNumberButton(numberText)
     }
     
+    /**
+     IBAction triggered when an operator button is tapped. It retrieves the title of the tapped button,
+     representing a mathematical operator, and informs the calculator instance to process the tapped operator.
+     
+     - Parameter sender: The UIButton that triggered the action.
+    */
     @IBAction func tappedOperatorButton(_ sender: UIButton) {
         guard let mathOperator = sender.title(for: .normal) else { return }
         
         calculator.tappedOperatorButton(mathOperator)
     }
     
+    /**
+     IBAction triggered when the equal button is tapped. It informs the calculator instance to perform
+     the calculation based on the entered expression.
+     
+     - Parameter sender: The UIButton that triggered the action.
+    */
     @IBAction func tappedEqualButton(_ sender: UIButton) {
         calculator.tappedEqualButton()
     }
--- CountOnMe/Model/Calculator.swift
+++ CountOnMe/Model/Calculator.swift
@@ -8,35 +8,52 @@
 
 import Foundation
 
+// MARK: - CalculatorDelegate
+
+/// A protocol that defines delegate methods for updating display text and displaying alerts in the Calculator module.
 protocol CalculatorDelegate {
     func updateDisplay(text: String)
     func displayAlert(message: String)
 }
 
 final class Calculator {
+    
+    // MARK: - Properties
+    
+    /// The delegate that will be notified of updates and alters from the calculator.
     var delegate: CalculatorDelegate?
     
+    /// The current text representing the user's input and calculated expression.
     var text: String = ""
     
+    /// An array of elements obtained by splitting the 'text' at spaces.
     var elements: [String] {
         text.split(separator: " ").map { "\($0)" }
     }
+    
+    /// Checks if the expression has enough elements to perform calculations.
     var expressionHaveEnoughElement: Bool {
         return elements.count >= 3
     }
     
+    /// Checks if an operator can be added to the expression.
     var canAddOperator: Bool {
         return elements.last != "+" && elements.last != "-"
     }
     
+    /// Checks if the expression contains a result.
     var expressionHaveResult: Bool {
         return text.firstIndex(of: "=") != nil
     }
     
+    /// Checks if the expression is currently correct and can be evaluated.
     var expressionIsCorrect: Bool {
         return (elements.last != "+") && (elements.last != "-")
     }
     
+    // MARK: - Methods
+    
+    /// Handles the tap event on a number button
     func tappedNumberButton(_ number: String) {
         if expressionHaveResult {
             text = ""
@@ -45,6 +62,7 @@ final class Calculator {
         delegate?.updateDisplay(text: text)
     }
     
+    /// Handles the tap event on an operator button
     func tappedOperatorButton(_ mathOperator: String) {
         if canAddOperator {
             text.append(" \(mathOperator) ")
@@ -54,10 +72,12 @@ final class Calculator {
         }
     }
     
+    /// Handles the tap event on the equal button
     func tappedEqualButton() {
         evaluateExpression()
     }
     
+    /// Evaluate the expression and updates the display
     func evaluateExpression(){
         // Create local copy of operations
         var operationsToReduce = elements
@@ -88,6 +108,16 @@ final class Calculator {
         delegate?.updateDisplay(text: text)
     }
         
+    /**
+     Calculates the result of a mathematical operation between two numbers.
+         
+     - Parameters:
+        - left: The left operand of the operation. Type : Double
+        - operand: The operator indicating the type of operation. Type : String
+        - right: The right operand of the operation. Type : Double
+     
+     - Returns: The result of the operation. Type : Double
+     */
     private func calcul(left: Double, operand: String, right: Double) -> Double {
         var result: Double
         switch operand {
@@ -100,25 +130,63 @@ final class Calculator {
         return result
     }
         
+    /**
+    Calculates the result of the highest priority operation in the expression.
+     
+     - Parameter expression: The array of expression elements. Type : [String]
+     - Returns: The result of the highest priority operation. Type : Double
+    */
     private func priorityCalculate(expression: [String]) -> Double {
-        var result: Double = 0.0
-        
-        if expression.contains("x") {
+        let result: Double = 0.0
+        if !expression.contains("+") && !expression.contains("-") {
+            if let result = formCalculation(expression: expression, index: 1) {
+                return result
+            }
+        } else if expression.contains("+") || expression.contains("-") && expression.contains("x") && expression.contains("÷") {
+            if let indexMultiplication = expression.firstIndex(of: "x"), let indexDivision = expression.firstIndex(of: "÷") {
+                let index = min(indexMultiplication, indexDivision)
+                if let result = formCalculation(expression: expression, index: index) {
+                    return result
+                }
+            }
+        } else if expression.contains("x") {
             if let index = expression.firstIndex(of: "x") {
-                result = formCalculation(expression: expression, index: index)
+                if let result = formCalculation(expression: expression, index: index) {
+                    return result
+                }
             }
         } else if expression.contains("÷") {
             if let index = expression.firstIndex(of: "÷") {
-                result = formCalculation(expression: expression, index: index)
+                if let result = formCalculation(expression: expression, index: index) {
+                    return result
+                }
             }
         } else {
-            result = formCalculation(expression: expression, index: 1)
+            if let result = formCalculation(expression: expression, index: 1) {
+                return result
+            }
         }
         return result
     }
         
+    /**
+     Removes the elements of a processed operation and inserts the result into the expression.
+     
+     - Parameters:
+        - expression: The inout array of expression elements. Type : [String]
+        - result: The result of the processed operation. Type : Double
+     - Returns: The modified expression array. Type [String]
+    */
     private func removeElement(expression: inout [String], result: Double) -> [String]{
-        if expression.contains("x") {
+        if !expression.contains("+") && !expression.contains("-") {
+            expression = Array(expression.dropFirst(3))
+            expression.insert("\(result)", at: 0)
+        } else if expression.contains("+") || expression.contains("-") && expression.contains("x") && expression.contains("÷") {
+            if let indexMultiplication = expression.firstIndex(of: "x"), let indexDivision = expression.firstIndex(of: "÷") {
+                let index = min(indexMultiplication, indexDivision)
+                updateExpression(&expression, atIndex: index, withResult: result)
+            }
+        } else if expression.contains("x") {
             if let index = expression.firstIndex(of: "x") {
                 updateExpression(&expression, atIndex: index, withResult: result)
             }
@@ -133,15 +201,32 @@ final class Calculator {
         return expression
     }
     
-    func formCalculation(expression: [String], index: Int) -> Double {
-        let left = Double(expression[index - 1])!
+    /**
+     Performs a calculation based on the elements at a specified index in the expression.
+     - Parameters:
+        - expression: The array of expression elements.
+        - index: The index indicating the position of the operator.
+     
+     - Returns: The result of the calculation.
+    */
+    func formCalculation(expression: [String], index: Int) -> Double? {
+        guard let left = Double(expression[index - 1]), let right = Double(expression[index + 1]) else {
+            return nil
+        }
         let operand = expression[index]
-        let right = Double(expression[index + 1])!
         let result = calcul(left: left, operand: operand, right: right)
         
         return result
     }
     
+    /**
+     Updates the expression array after a processed operation with the result.
+     
+     - Parameters:
+        - expression: The inout array of expression elements. Type : [String]
+        - index: The index indicating the position of the operator. Type : Int
+        - result: The result of the processed operation. Type : Double
+     */
     func updateExpression(_ expression: inout [String], atIndex index: Int, withResult result: Double) {
         expression.remove(at: index)
         expression.remove(at: index)
